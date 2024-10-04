074ae185bf02d030bc8126faedd19f52876aea9e
martinondiwa <surekondiwa@gmail.com>
2024-10-04 11:10:10 +0300

    ad

--- index.php
+++ index.php
@@ -1,48 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nairobi School of Business and Technology</title>
     <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
 
-<Nav>
-    <label class="logo">
-        Nairobi School of Business and Technology
-    </label>
+    <Nav>
+        <label class="logo">
+            Nairobi School of Business and Technology
+        </label>
 
-    <ul >
-        <li><a href="">Home</a></li>
-        <li><a href="">About Us</a></li>
-        <li>
-        <div class="btn-group">
-  <button type="button" class="btn btn-danger">Academic Programmes</button>
-  <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    <span class="sr-only">Explore our programs</span>
-  </button>
-  <div class="dropdown-menu">
-    <a class="dropdown-item" href="#">Professional Certifications</a>
-    <a class="dropdown-item" href="#">Technology and IT Skills</a>
-    <a class="dropdown-item" href="#">Creative Arts and Design</a>
-    <a class="dropdown-item" href="#">Vocational and Technical Skills</a>
-    <a class="dropdown-item" href="#"> Specialized Business Training</a>
-    <div class="dropdown-divider"></div>
-    <a class="dropdown-item" href="#">Separated link</a>
-  </div>
-</div>
-      
-        </li>
-        <li><a href="">Blog</a></li>
-        <li><a href="">Admission</a></li>
-        <li><a href="">Apply Online</a></li>
-        <li><a href="">Portal</a></li>
-    </ul>
-</Nav>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About Us</a></li>
+            <li>
+                <div class="btn-group">
+                    <button type="button" class="btn btn-danger">Academic Programmes</button>
+                    <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="sr-only">Explore our programs</span>
+                    </button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Professional Certifications</a>
+                        <a class="dropdown-item" href="#">Technology and IT Skills</a>
+                        <a class="dropdown-item" href="#">Creative Arts and Design</a>
+                        <a class="dropdown-item" href="#">Vocational and Technical Skills</a>
+                        <a class="dropdown-item" href="#"> Specialized Business Training</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Separated link</a>
+                    </div>
+                </div>
+
+            </li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Admission</a></li>
+            <li><a href="">Apply Online</a></li>
+            <li><a href="">Portal</a></li>
+        </ul>
+    </Nav>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
