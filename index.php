203218a6aaeb7b0b075780ea45bf6368b6af8732
martinondiwa <surekondiwa@gmail.com>
2024-10-04 13:12:28 +0300

    asdd

--- index.php
+++ index.php
@@ -121,7 +121,7 @@
     <div>
         <div class="row">
             <div class="col-md-4">
-                <img src="/images/image.PNG" alt="">
+                <img src="./images/image.PNG" alt="">
             </div>
             <div class="col-md-8">
                 <h1>Welcome to Kenya School of Business & Technology</h1>
