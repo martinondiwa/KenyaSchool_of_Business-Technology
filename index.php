52e22ea4f72493b4ee7da456d035d1d70a9f01eb
martinondiwa <surekondiwa@gmail.com>
2024-10-04 09:28:04 +0300

    ad

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nairobi School of Business and Technology</title>
+</head>
+<body>
+    <h1>Welcome to Nairobi School of Business and Technlogy</h1>
+</body>
+</html>
