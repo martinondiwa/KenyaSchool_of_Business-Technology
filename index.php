2ade249bf7c485d3b603d100d295e2449f1ced80
martinondiwa <surekondiwa@gmail.com>
2024-10-04 12:11:11 +0300

    asd

--- index.php
+++ index.php
@@ -113,6 +113,10 @@
         </ul>
     </Nav>
 
+    <div class="Section1">
+      <image src=""/>
+    </div>
+
     <!-- Bootstrap JS and Popper.js for the dropdown functionality -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
