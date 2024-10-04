05c9953b1313b73a20e4fd9b63cf1d7958bf6189
martinondiwa <surekondiwa@gmail.com>
2024-10-04 09:59:36 +0300

    ad

--- index.php
+++ index.php
@@ -13,12 +13,20 @@
     </label>
 
     <ul>
-        <li><a href="">Home</a></li>
-        <li><a href="">Home</a></li>
-        <li><a href="">Home</a></li>
-        <li><a href="">Home</a></li>
-        <li><a href="">Home</a></li>
-        <li><a href="">Home</a></li>
+        <li><a href="">About Us</a></li>
+        <li><a href="">Courses</a>
+          <Ul>
+            <li> <a href=""> Professional Certifications</a></li>
+            <li> <a href=""> Technology and IT Skills</a></li>
+            <li> <a href=""> Creative Arts and Design</a></li>
+            <li> <a href=""> Vocational and Technical Skills</a></li>
+            <li> <a href=""> Specialized Business Training</a></li>
+          </Ul>
+        </li>
+        <li><a href="">Blog</a></li>
+        <li><a href="">Contact US</a></li>
+        <li><a href="">Admission</a></li>
+        <li><a href="">login</a></li>
     </ul>
 </Nav>
 
