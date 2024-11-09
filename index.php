6731311cefc4d7735280c8fc129ff6d78c5f5e52
martinondiwa <surekondiwa@gmail.com>
2024-11-09 22:24:46 +0300

    asd

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Kenya School of Business & Technology</title>
     <link rel="stylesheet" href="style.css">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
