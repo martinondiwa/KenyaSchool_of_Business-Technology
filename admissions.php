6728abd719041de0dd85ff72d1fc35bbb229640f
martinondiwa <surekondiwa@gmail.com>
2024-11-23 18:01:44 +0300

    as

--- admissions.php
+++ admissions.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admissions | Kenya School of Business and IT</title>
 
+    <link rel="stylesheet" href="admissions.css">
     <link rel="stylesheet" href="style.css">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
