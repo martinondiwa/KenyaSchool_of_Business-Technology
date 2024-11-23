62b641ba76f2414c4c1542bc48a5f33cf430a370
martinondiwa <surekondiwa@gmail.com>
2024-11-23 19:26:52 +0300

    as

--- Home.php
+++ Home.php
@@ -6,15 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kenya School of Business & Technology</title>
 
-   
+
     <link rel="stylesheet" href="homestyle.css">
     <link rel="stylesheet" href="style.css">
 
-       <!-- Bootstrap CSS -->
+    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
+
+<style>
 /* Top Navbar Styles */
 .top-nav {
     display: flex;
@@ -106,12 +108,10 @@ img {
 </Style>
 
 <body>
-
-
-    <!--Nav Bar-->
-    <section>
-     <!-- Top Navbar. -->
-     <div class="top-nav">
+    <div class="navbar-container">
+        
+        <!-- Top Navbar. -->
+        <div class="top-nav">
             <div class="left-section">
                 <div class="icon-text">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -119,7 +119,7 @@ img {
                         <path
                             d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
                     </svg>
-                    <span style="padding-left: 10px">Phone: 0745 344 881 </span>
+                    <span style="padding-left: 10px">Phone: 0745 334 881 </span>
                 </div>
                 <div class="icon-text">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -127,7 +127,7 @@ img {
                         <path
                             d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
                     </svg>
-                    <span style="padding-left: 10px">Email: geo@subomapgeosystems.com</span>
+                    <span style="padding-left: 10px">Email: info@ksti.com</span>
                 </div>
                 <div class="icon-text">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -150,7 +150,7 @@ img {
             </div>
         </div>
 
-<!-- Main Nav-->
+        <!-- Main Nav-->
         <Nav>
             <nav class="main-nav">
                 <label class="logo"><img src="./images/logoo.PNG" alt="Logo" class="logo-img"></label>
