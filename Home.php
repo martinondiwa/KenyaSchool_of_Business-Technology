01cfa057a2cc9b498cf24c1db5023dafcaa29f7f
martinondiwa <surekondiwa@gmail.com>
2024-11-23 19:19:35 +0300

    ass

--- Home.php
+++ Home.php
@@ -10,7 +10,11 @@
     <link rel="stylesheet" href="homestyle.css">
     <link rel="stylesheet" href="style.css">
 
-    
+       <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
 /* Top Navbar Styles */
 .top-nav {
     display: flex;
@@ -100,13 +104,6 @@ img {
 
 
 </Style>
-
-
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
 
 <body>
 
