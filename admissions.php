37a3e047773e53d773c9f36c6ce1476edc59b9c4
martinondiwa <surekondiwa@gmail.com>
2024-12-14 23:26:36 +0300

    asd

--- admissions.php
+++ admissions.php
@@ -176,7 +176,7 @@
     </script>
 
 
-    <!-- Bootstrap JS and Popper.js for the dropdown functionality -->
+    <!-- Bootstrap JS and Popper.js for dropdown functionality -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
     </script>
