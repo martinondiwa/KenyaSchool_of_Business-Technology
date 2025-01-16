7e70fc1eac3ae2514a6238560ebdeb7be9507115
martinondiwa <surekondiwa@gmail.com>
2025-01-16 23:07:55 +0300

    adfg

--- media_desk.php
+++ media_desk.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Nairobi School of Business and Technology</title>
-  <!-- Bootstrap CSS -->
+  <!-- Bootstrap  -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <style>
     /* Styles */
