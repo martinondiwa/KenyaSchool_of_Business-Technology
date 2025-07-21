d905a8a67e12d127c9fa07c102dd8e560955261d
martinondiwa <surekondiwa@gmail.com>
2025-07-21 23:50:04 +0300

    sdfg

--- media_desk.php
+++ media_desk.php
@@ -153,7 +153,7 @@
     <p>Follow us on social media for updates and opportunities!</p>
   </footer>
 
-  <!-- Bootstrap & JS  dependencies -->
+  <!-- Bootstrap & JS  dependencies erryu-->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
 </body>
