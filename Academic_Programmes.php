47f31403baa20bf74573f30d30f2eb2112d28431
Martin Ondiwa <96140915+martinondiwa@users.noreply.github.com>
2026-02-09 22:11:16 -0500

    Update title to 'Academic Programs'

--- Academic_Programmes.php
+++ Academic_Programmes.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Programs and Academics</title>
+    <title>Academic Programs </title>
 
     <link rel="stylesheet" href="style.css">
     <!-- Bootstrap CSS -->
