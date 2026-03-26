e4cc5bf4cbc97ce792ee8685c6914e5d5e4e7c58
Martin Ondiwa <96140915+martinondiwa@users.noreply.github.com>
2026-03-25 22:55:55 -0500

    Home.php

--- Home.php
+++ Home.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kenya School of Business & Technology</title>
+    <title>Kenya School of Business and Technology</title>
 
 
     <link rel="stylesheet" href="homestyle.css">
